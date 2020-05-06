19c6596f5bffe36b2706bfaf92cfdb15b023f9b8
glacials <qhiiyr@gmail.com>
2020-05-06 12:09:12 -0700

    Fix deploys

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-git subtree push --prefix dist https://glacials:${GITHUB_TOKEN}@github.com/glacials/twos.dev.git gh-pages
+
+mkdir -p build
+yarn install
+yarn build
+cd build
+git init
+git remote add origin https://glacials:${GITHUB_TOKEN}@github.com/glacials/twos.dev
+git checkout -b gh-pages
+git add .
+git -c 'user.name=YourBase' -c 'user.email=ben@yourbase.io' commit -am 'Automatic deploy by YourBase'
+git push origin gh-pages --force
+cd ../
