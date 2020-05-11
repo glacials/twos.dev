40a72eb2a6cf882f00204c063e6681e6769356b9
glacials <qhiiyr@gmail.com>
2020-05-11 09:15:33 -0700

    Remove Calendly link

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="mb-4">
     <div class="mb-1">
-      <b-button href="https://calendly.com/glacials" class="d-print-none" variant="primary">
-        Schedule a call
-      </b-button>
-      <b-button href="mailto:qhiiyr@gmail.com">
+      <b-button href="mailto:qhiiyr@gmail.com" variant="primary">
         Email me
         <span class="d-none d-print-inline">
           &mdash; qhiiyr@gmail.com
         </span>
       </b-button>
-    </div>
-    <div>
-      <b-button href="../ben-carlsson-resume-2020-03.pdf" variant="link">
-        <font-awesome-icon :icon="['fas', 'download']" />
-        Download my resume
+      <b-button href="../ben-carlsson-resume-2020-03.pdf">
+        View my resume
       </b-button>
     </div>
   </div>
