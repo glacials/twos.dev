d4b127c8e540b9d92a5d5f98b6cef42da5c26edd
glacials <ben@twos.dev>
2022-05-17 12:00:12 -0700

    More automation

--- devops/build.js
+++ devops/build.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fsSync = require('fs')
 const fs = require('fs/promises')
 const os = require('os')
@@ -6,44 +8,64 @@ const yaml = require('yaml')
 
 const Markdoc = require('@markdoc/markdoc')
 
-const sourceDir = `${os.homedir()}/Library/Mobile\ Documents/27N4MQEA55\~pro\~writer/Documents/Published`;
-const destinationDir = './dist';
+const writingDir = `${os.homedir()}/Library/Mobile\ Documents/27N4MQEA55\~pro\~writer/Documents/Published`;
+const buildDir = './dist';
 
 (async () => {
+  // Anything in here will be waited on before the process exits.
+  const promises = []
+
   try {
-    if (fsSync.existsSync(destinationDir)) {
-      await fs.rm(destinationDir, {recursive: true})
+    if (fsSync.existsSync(buildDir)) {
+      await fs.rm(buildDir, {recursive: true})
     }
-    await fs.mkdir(destinationDir)
+    await fs.mkdir(buildDir)
 
     // Add .md to any file without it
-    await Promise.all((await fs.readdir(sourceDir)).map(filename => {
+    await Promise.all((await fs.readdir(writingDir)).map(filename => {
       if (filename.substring(filename.length - ".md".length) == ".md") {
         return Promise.resolve()
       }
 
       if (filename.substring(filename.length - ".txt".length) == ".txt") {
         return fs.rename(
-          `${sourceDir}/${filename}`,
-          `${sourceDir}/${filename.substring(0, filename.length - ".txt".length)}.md`,
+          `${writingDir}/${filename}`,
+          `${writingDir}/${filename.substring(0, filename.length - ".txt".length)}.md`,
         )
       }
 
-      return fs.rename(`${sourceDir}/${filename}`, `${sourceDir}/${filename}.md`)
+      return fs.rename(`${writingDir}/${filename}`, `${writingDir}/${filename}.md`)
     }))
+
+    const header = fs.readFile(`./src/_header.html`)
+    const footer = fs.readFile(`./src/_footer.html`)
+    promises.push(fs.copyFile('./src/index.html', `${buildDir}/index.html`))
+    promises.push(fs.copyFile('./src/style.css', `${buildDir}/style.css`))
 
     // Re-glob after renames 
     // TODO: Just remember the renames
-    const files = (await fs.readdir(sourceDir)).map(filename => {
-      return fs.readFile(`${sourceDir}/${filename}`, {encoding: 'utf8'}).then(body => {
+    const files = (await fs.readdir(writingDir)).map(filename => {
+      return fs.readFile(`${writingDir}/${filename}`, {encoding: 'utf8'}).then(body => {
         const ast = Markdoc.parse(body)
         const content = Markdoc.transform(ast)
         const html = Markdoc.renderers.html(content)
         if (!ast.attributes.frontmatter) {
+          promises.push(fs.writeFile(
+            `${writingDir}/${filename}`,
+            `---\nerror: Must specify shortname\n---\n\n${body}`,
+          ))
           throw `Frontmatter not found for "${filename}"`
         }
+        const frontmatter = yaml.parse(ast.attributes.frontmatter)
+        if (!frontmatter.shortname) {
+          frontmatter.error = "Must specify shortname"
+          promises.push(fs.writeFile(
+            `${writingDir}/${filename}`,
+            body.replace(ast.attributes.frontmatter + '\n', `${yaml.stringify(frontmatter)}`),
+          ))
+        }
         return {
-          shortname: yaml.parse(ast.attributes.frontmatter).shortname,
+          shortname: frontmatter.shortname,
           filename: filename.substring(0, filename.length - ".md".length),
           title: filename.substring(0, filename.length - ".md".length),
           body: html,
@@ -53,12 +75,14 @@ const destinationDir = './dist';
 
     await Promise.all(
       files.map(async file => fs.writeFile(
-        `${destinationDir}/${(await file).shortname}.html`,
-        (await file).body,
+        `${buildDir}/${(await file).shortname}.html`,
+        (await header) + (await file).body + (await footer),
       ))
     )
+    await Promise.all(promises)
   } catch (e) {
     console.error(e)
+    await Promise.all(promises)
     process.exit(1)
   }
 })()
