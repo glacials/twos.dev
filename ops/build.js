78bc39fc1792b086a6f23a686a676f430afb1ee0
glacials <ben@twos.dev>
2022-05-17 20:09:38 -0700

    Slight promise restructuring of build.js

--- ops/build.js
+++ ops/build.js
@@ -21,73 +21,60 @@ const dist = './dist';
     }
     await fs.mkdir(dist)
 
-    const header = fs.readFile(`${src}/_header.html`)
-    const footer = fs.readFile(`${src}/_footer.html`)
+    const header = fs.readFile(`${src}/_header.html`);
+    const footer = fs.readFile(`${src}/_footer.html`);
 
-    const files = (await fs.readdir(src)).map(async filename => {
-      if (filename.substring(filename.length - ".md") != ".md") {
-        return
+    (await fs.readdir(src)).filter(filename => {
+      return filename.substring(filename.length - ".md") == ".md"
+    }).map(filename => fs.readFile(`${src}/${filename}`, {encoding: 'utf8'}).then(async body => {
+      const stat = fs.stat(`${src}/${filename}`)
+      const ast = Markdoc.parse(body)
+      const content = Markdoc.transform(ast)
+      let html = Markdoc.renderers.html(content)
+      const originalFrontmatter = ast.attributes.frontmatter
+      if (!ast.attributes.frontmatter) {
+        ast.attributes.frontmatter = 'some: frontmatter\n'
       }
-
-      return fs.readFile(`${src}/${filename}`, {encoding: 'utf8'}).then(async body => {
-        const stat = fs.stat(`${src}/${filename}`)
-        const ast = Markdoc.parse(body)
-        const content = Markdoc.transform(ast)
-        let html = Markdoc.renderers.html(content)
-        const originalFrontmatter = ast.attributes.frontmatter
-        if (!ast.attributes.frontmatter) {
-          ast.attributes.frontmatter = 'some: frontmatter\n'
+      const frontmatter = yaml.parse(ast.attributes.frontmatter) || {}
+      let error = false
+      if (!frontmatter.filename) {
+        frontmatter.filename = "TODO"
+        error = true
+      }
+      const date = new Date()
+      if (!frontmatter.date) {
+        frontmatter.date = "TODO"
+        error = true
+      } else {
+        const parts = frontmatter.date.split('-')
+        date.setYear(parts[0])
+        if (parts[1]) {
+          date.setMonth(parts[1] - 1)
         }
-        const frontmatter = yaml.parse(ast.attributes.frontmatter) || {}
-        let error = false
-        if (!frontmatter.filename) {
-          frontmatter.filename = "TODO"
-          error = true
+        if (parts[2]) {
+          date.setDate(parts[2])
         }
-        const date = new Date()
-        if (!frontmatter.date) {
-          frontmatter.date = "TODO"
-          error = true
-        } else {
-          const parts = frontmatter.date.split('-')
-          date.setYear(parts[0])
-          if (parts[1]) {
-            date.setMonth(parts[1] - 1)
-          }
-          if (parts[2]) {
-            date.setDate(parts[2])
-          }
-          let dateStr = `${date.toLocaleString('default', {month: 'long'})} ${date.getFullYear()}`
-          if ((await stat).mtime.getMonth() != date.getMonth() || (await stat).mtime.getFullYear() != date.getFullYear()) {
-            dateStr += `; last updated ${(await stat).mtime.toLocaleString('default', {month: 'long'})} ${(await stat).mtime.getFullYear()}`
-          }
-          html = html.replace('</h1>', `</h1><p>${dateStr}</p>`)
+        let dateStr = `${date.toLocaleString('default', {month: 'long'})} ${date.getFullYear()}`
+        if ((await stat).mtime.getMonth() != date.getMonth() || (await stat).mtime.getFullYear() != date.getFullYear()) {
+          dateStr += `; last updated ${(await stat).mtime.toLocaleString('default', {month: 'long'})} ${(await stat).mtime.getFullYear()}`
         }
-        if (error) {
-          promises.push(fs.writeFile(
-            `${src}/${filename}`,
-            `---\n${yaml.stringify(frontmatter)}---\n\n` + body.replace(
-              `---\n${originalFrontmatter}\n---\n\n`,
-              '',
-            )
-          ))
-        }
-        return {
-          desiredFilename: frontmatter.filename || 'TODO.html',
-          date,
-          filename: filename.substring(0, filename.length - ".md".length),
-          title: filename.substring(0, filename.length - ".md".length),
-          body: html,
-        }
-      })
-    }).filter(f => f)
-
-    await Promise.all(
-      files.map(async file => fs.writeFile(
-        `${dist}/${(await file).desiredFilename}`,
-        (await header) + (await file).body + (await footer),
+        html = html.replace('</h1>', `</h1><p>${dateStr}</p>`)
+      }
+      if (error) {
+        promises.push(fs.writeFile(
+          `${src}/${filename}`,
+          `---\n${yaml.stringify(frontmatter)}---\n\n` + body.replace(
+            `---\n${originalFrontmatter}\n---\n\n`,
+            '',
+          )
+        ))
+      }
+      promises.push(fs.writeFile(
+        `${dist}/${frontmatter.filename}`,
+        (await header) + html + (await footer),
       ))
-    )
+    }))
+
     await Promise.all(promises)
     fsSync.copySync('./public', './dist')
   } catch (e) {
