fc4bc3afb013059028b610b21d35f57fc2e56e8f
glacials <ben@twos.dev>
2022-05-17 19:44:52 -0700

    Massage iA files correctly in sync

--- ops/sync.js
+++ ops/sync.js
@@ -3,6 +3,7 @@
 
 import replaceExt from 'replace-ext'
 import fs from 'fs-extra'
+import Markdoc from '@markdoc/markdoc'
 import simpleGit from 'simple-git'
 
 const ia = `${os.homedir()}/Library/Mobile\ Documents/27N4MQEA55\~pro\~writer/Documents/Published`;
@@ -13,9 +14,9 @@ const src = './src'
     const git = simpleGit()
     await git.pull()
 
-    // Change any .txt or no-extension iA file to .md
+    // Change any .txt iA file to .md
     await Promise.all((await fs.readdir(ia)).map(filename => {
-      if (filename.substring(filename.length - ".txt".length) == ".txt" || filename.indexOf('.') < 0) {
+      if (filename.substring(filename.length - ".txt".length) == ".txt") {
         return fs.rename(
           `${ia}/${filename}`,
           `${ia}/${replaceExt(`${ia}/${filename}`, '.md')}`,
@@ -23,6 +24,40 @@ const src = './src'
       }
 
       return Promise.resolve()
+    }))
+
+    // Add frontmatter to any iA file without it
+    await Promise.all((await fs.readdir(ia)).map(filename => {
+      const requiredProperties = {
+        date: "changeme",
+        filename: "changeme",
+      }
+
+      const ast = Markdoc.parse(body)
+      const frontmatterStr = ast.attributes.frontmatter
+
+      if (!frontmatterStr) {
+        const body = await fs.readFile(`${ia}/${filename}`)
+        return fs.writeFile(
+          `${ia}/${filename}`,
+          `---\n${yaml.stringify(requiredProperties)}---\n\n${body}`
+        )
+      }
+
+      let needsUpdate = false
+      const frontmatter = yaml.parse(frontmatterStr)
+      for (const [key, val] of Object.entries(requiredProperties)) {
+        if (!frontmatter[key]) {
+          needsUpdate = true
+          frontmatter[key] = val
+        }
+      }
+      if (needsUpdate) {
+        return fs.writeFile(
+          `${ia}/${filename}`,
+          `---\n${yaml.stringify(frontmatter)}---\n\n${body.replace(frontmatterStr, '')}`,
+        );
+      }
     }))
 
     fs.copySync(ia, src)
