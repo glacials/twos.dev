d6e181c88b4ca0ee2dce84e06a4ed12aadb5ea8d
glacials <ben@twos.dev>
2022-06-05 15:23:33 -0700

    Refactor HTML & Markdown building to common code

--- cmd/build_templates.go
+++ cmd/build_templates.go
@@ -4,12 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 
 	"github.com/glacials/twos.dev/cmd/frontmatter"
 )
@@ -33,35 +33,13 @@ func NewTemplateBuilder() (templateBuilder, error) {
 		return templateBuilder{}, fmt.Errorf("can't create essay template: %w", err)
 	}
 
-	buildHTMLFile := func(src, dst string) error {
-		f, err := os.Open(src)
-		if err != nil {
-			// TODO: Clean this up; Prettier autoformatting seems to remove the file
-			// and quickly place it back, so ignore th error.
-			log.Printf(
-				fmt.Errorf(
-					"can't open HTML file at `%s` for building: %w",
-					src,
-					err,
-				).Error(),
-			)
-			return nil
-		}
-
-		matter, body, err := frontmatter.Parse(f)
-		if err != nil {
-			return fmt.Errorf("can't get frontmatter from HTML file: %w", err)
-		}
-
+	buildHTMLStream := func(src io.Reader, dst string, matter frontmatter.Matter) error {
 		if err := os.MkdirAll(dst, 0755); err != nil {
 			return fmt.Errorf("can't make destination directory `%s`: %w", dst, err)
 		}
 
 		if matter.Filename == "" {
-			if !strings.HasSuffix(filepath.Base(src), ".html") {
-				return fmt.Errorf("non-HTML file %s must have a filename attribute in frontmatter", src)
-			}
-			matter.Filename = filepath.Base(src)
+			return fmt.Errorf("file frontmatter has no filename attribute")
 		}
 
 		destinationFilePath := filepath.Join(dst, matter.Filename)
@@ -83,9 +61,14 @@ func NewTemplateBuilder() (templateBuilder, error) {
 			updatedAt = matter.CreatedAt.Format("2006 January")
 		}
 
+		body, err := io.ReadAll(src)
+		if err != nil {
+			return fmt.Errorf("can't read body from stream: %w", err)
+		}
+
 		v := htmlFileVars{
 			Body:  template.HTML(body),
-			Title: filepath.Base(src)[0 : len(filepath.Base(src))-len(".html")],
+			Title: "twos.dev", // TODO: Strip from first <h1> in HTML
 			SourceURL: fmt.Sprintf(
 				"https://github.com/glacials/twos.dev/blob/main/%s",
 				src,
@@ -97,6 +80,37 @@ func NewTemplateBuilder() (templateBuilder, error) {
 
 		if err := essay.Execute(htmlFile, v); err != nil {
 			return fmt.Errorf("can't execute essay template: %w", err)
+		}
+
+		return nil
+	}
+
+	buildHTMLFile := func(src, dst string) error {
+		f, err := os.Open(src)
+		if err != nil {
+			// TODO: Clean this up; Prettier autoformatting seems to remove the file
+			// and quickly place it back, so ignore th error.
+			log.Printf(
+				fmt.Errorf(
+					"can't open HTML file at `%s` for building: %w",
+					src,
+					err,
+				).Error(),
+			)
+			return nil
+		}
+
+		matter, body, err := frontmatter.Parse(f)
+		if err != nil {
+			return fmt.Errorf("can't get frontmatter from Markdown file: %w", err)
+		}
+
+		if matter.Filename == "" {
+			matter.Filename = filepath.Base(src)[0 : len(filepath.Base(src))-len(".html")]
+		}
+
+		if err := buildHTMLStream(bytes.NewBuffer(body), dst, matter); err != nil {
+			return fmt.Errorf("can't build HTML stream: %w", err)
 		}
 
 		return nil
@@ -121,7 +135,7 @@ func NewTemplateBuilder() (templateBuilder, error) {
 			)
 		}
 
-		matter, _, err := frontmatter.Parse(f)
+		matter, body, err := frontmatter.Parse(f)
 		if err != nil {
 			return fmt.Errorf("can't get frontmatter from Markdown file: %w", err)
 		}
@@ -130,32 +144,10 @@ func NewTemplateBuilder() (templateBuilder, error) {
 			return fmt.Errorf("can't make destination directory `%s`: %w", dst, err)
 		}
 
-		destinationFilePath := filepath.Join(dst, matter.Filename)
-		htmlFile, err := os.Create(destinationFilePath)
-		if err != nil {
-			return fmt.Errorf(
-				"can't render HTML to `%s` from template for `%s`: %w",
-				destinationFilePath,
-				src,
-				err,
-			)
+		if err := buildHTMLStream(bytes.NewBuffer(body), dst, matter); err != nil {
+			return fmt.Errorf("can't build HTML stream: %w", err)
 		}
 
-		v := htmlFileVars{
-			Body:  template.HTML(renderedHTML.String()),
-			Title: filepath.Base(src)[0 : len(filepath.Base(src))-len(".md")],
-			SourceURL: fmt.Sprintf(
-				"https://github.com/glacials/twos.dev/blob/main/%s",
-				src,
-			),
-
-			CreatedAt: matter.CreatedAt.Format("2006 January"),
-			UpdatedAt: matter.UpdatedAt.Format("2006 January"),
-		}
-
-		if err := essay.Execute(htmlFile, v); err != nil {
-			return fmt.Errorf("can't execute essay template: %w", err)
-		}
 		return nil
 	}
 
