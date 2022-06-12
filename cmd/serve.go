07c76587f239c98b6a7e252d48a9c8ede3dd5cb1
glacials <ben@twos.dev>
2022-06-12 16:12:51 -0700

    Minor lint

--- cmd/serve.go
+++ cmd/serve.go
@@ -75,7 +75,7 @@ var serveCmd = &cobra.Command{
 		}
 		stop := make(chan struct{})
 
-		c := make(chan os.Signal)
+		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 		go func() {
 			select {
