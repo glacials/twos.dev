a2b49fb971af3a09380923e7d45ad0606f0f17af
glacials <ben@twos.dev>
2022-07-06 19:56:42 -0700

    Allow winter freeze to take N args

--- winter/cmd/freeze.go
+++ winter/cmd/freeze.go
@@ -12,7 +12,7 @@ import (
 )
 
 var freezeCmd = &cobra.Command{
-	Use:   "freeze shortname",
+	Use:   "freeze shortname...",
 	Short: "Turn a warm file cold",
 	Long: wrap(`
 		Convert the warm document specified into a cold document. Run this when a
@@ -25,68 +25,69 @@ var freezeCmd = &cobra.Command{
 		explicitly in frontmatter or implicitly in filename) and moves it to
 		src/cold.
 	`),
-	Args: cobra.ExactArgs(1),
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		shortname := args[0]
-		s, err := winter.NewSubstructure(winter.Config{})
-		if err != nil {
-			return err
-		}
-
-		document := s.DocByShortname(shortname)
-		if document == nil {
-			return fmt.Errorf("cannot find document with shortname `%s`", shortname)
-		}
-
-		oldpath := document.SrcPath
-		newpath := filepath.Join("src", "cold", filepath.Base(document.SrcPath))
-
-		// The directory to remove warm files from, in addition to src/warm, so that
-		// the file doesn't just sync back to src/warm after removal. TODO: Make
-		// configurable.
-		warmSourceOfTruth := "/Users/glacials/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/Published/"
-		if _, err := os.Stat(warmSourceOfTruth); err != nil {
-			// Allow dir to not exist, since it's hardcoded to my dir ;)
-			if !os.IsNotExist(err) {
-				return err
-			}
-		} else {
-			rel, err := filepath.Rel("src/warm", oldpath)
+		for _, shortname := range args {
+			s, err := winter.NewSubstructure(winter.Config{})
 			if err != nil {
 				return err
 			}
-			if err := os.Remove(filepath.Join(warmSourceOfTruth, rel)); err != nil {
+
+			document := s.DocByShortname(shortname)
+			if document == nil {
+				return fmt.Errorf("cannot find document with shortname `%s`", shortname)
+			}
+
+			oldpath := document.SrcPath
+			newpath := filepath.Join("src", "cold", filepath.Base(document.SrcPath))
+
+			// The directory to remove warm files from, in addition to src/warm, so
+			// that the file doesn't just sync back to src/warm after removal. TODO:
+			// Make configurable.
+			warmSourceOfTruth := "/Users/glacials/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/Published/"
+			if _, err := os.Stat(warmSourceOfTruth); err != nil {
+				// Allow dir to not exist, since it's hardcoded to my dir ;)
+				if !os.IsNotExist(err) {
+					return err
+				}
+			} else {
+				rel, err := filepath.Rel("src/warm", oldpath)
+				if err != nil {
+					return err
+				}
+				if err := os.Remove(filepath.Join(warmSourceOfTruth, rel)); err != nil {
+					return err
+				}
+			}
+
+			g, err := git.New(git.Options{})
+			if err != nil {
 				return err
 			}
-		}
 
-		g, err := git.New(git.Options{})
-		if err != nil {
-			return err
-		}
+			ctx := context.TODO()
+			if err := os.Rename(oldpath, newpath); err != nil {
+				return err
+			}
+			fmt.Println("git add", oldpath, newpath)
+			if err := g.Add(
+				ctx,
+				[]git.Pathspec{git.Pathspec(oldpath), git.Pathspec(newpath)},
+				git.AddOptions{},
+			); err != nil {
+				return err
+			}
 
-		ctx := context.TODO()
-		if err := os.Rename(oldpath, newpath); err != nil {
-			return err
-		}
-		fmt.Println("git add", oldpath, newpath)
-		if err := g.Add(
-			ctx,
-			[]git.Pathspec{git.Pathspec(oldpath), git.Pathspec(newpath)},
-			git.AddOptions{},
-		); err != nil {
-			return err
-		}
+			fmt.Printf("git commit -m 'Freeze %s'\n", shortname)
+			if err := g.Commit(
+				ctx,
+				fmt.Sprintf("Freeze %s", shortname),
+				git.CommitOptions{},
+			); err != nil {
+				return err
+			}
 
-		fmt.Printf("git commit -m 'Freeze %s'\n", shortname)
-		if err := g.Commit(
-			ctx,
-			fmt.Sprintf("Freeze %s", shortname),
-			git.CommitOptions{},
-		); err != nil {
-			return err
 		}
-
 		return nil
 	},
 }
